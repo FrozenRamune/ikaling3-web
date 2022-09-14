11d00ac92c93752f39e762edc7382d0694998215
Huttou_Tansan <66063808+Huttou-Tansan@users.noreply.github.com>
2022-09-15 07:46:58 +0900

    Update token_get.js

--- token_get.js
+++ token_get.js
@@ -4,4 +4,12 @@ window.addEventListener('message', function (e) {
         return;
     }
     alert(e.data);
+    const params = {};
+    redirectURL.split('#')[1]
+        .split('&')
+        .forEach(str => {
+            const splitStr = str.split('=');
+            params[splitStr[0]] = splitStr[1];
+        });
+    alert(params.session_token_code);
 });
