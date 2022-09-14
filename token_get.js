b9e25d96fe67ce2851e085a0734989cdb5094c13
Huttou_Tansan <66063808+Huttou-Tansan@users.noreply.github.com>
2022-09-15 08:42:59 +0900

    Update token_get.js

--- token_get.js
+++ token_get.js
@@ -17,13 +17,13 @@ window.addEventListener('message', function (e) {
 });
 
 function session_token(session_token_code, session_token_code_verifier) {
+    const body = new FormData();
+    body.append('client_id', '71b963c1b7b6d119');
+    body.append('session_token_code', session_token_code);
+    body.append('session_token_code_verifier', session_token_code_verifier);
     fetch('https://accounts.nintendo.com/connect/1.0.0/api/session_token', {
         mode: 'no-cors',
         method: "POST",
-        body: JSON.stringify({
-            client_id: '71b963c1b7b6d119',
-            session_token_code: session_token_code,
-            session_token_code_verifier: session_token_code_verifier
-        })
+        body: body
     }).then((res)=> res.json()).then(console.log).catch(console.error);
 }
