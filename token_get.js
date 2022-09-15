e0a360de8629b950c8ff4416042a891af94751af
Huttou_Tansan <66063808+Huttou-Tansan@users.noreply.github.com>
2022-09-15 10:23:43 +0900

    Update token_get.js

--- token_get.js
+++ token_get.js
@@ -4,16 +4,20 @@ window.addEventListener('message', function (e) {
         return;
     }
     alert(e.data);
-    const params = {};
-    e.data.split('#')[1]
-        .split('&')
-        .forEach(str => {
-            const splitStr = str.split('=');
-            params[splitStr[0]] = splitStr[1];
-        });
-    alert(params.session_token_code);
-    alert(window.codeVerifier);
-    session_token(params.session_token_code, window.codeVerifier);
+    if(e.data.head == "session_token_code") {
+        const params = {};
+        e.data.body.split('#')[1]
+            .split('&')
+            .forEach(str => {
+                const splitStr = str.split('=');
+                params[splitStr[0]] = splitStr[1];
+            });
+        alert(params.session_token_code);
+        alert(window.codeVerifier);
+        session_token(params.session_token_code, window.codeVerifier);
+    } else if(e.data.head == "session_token_code") {
+        alert(e.data.body);
+    }
 });
 
 function session_token(session_token_code, session_token_code_verifier) {
