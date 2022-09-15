bed10c7416b2671a4268a9c713ab606be64e18a5
Huttou_Tansan <66063808+Huttou-Tansan@users.noreply.github.com>
2022-09-15 11:05:36 +0900

    Update token_get.js

--- token_get.js
+++ token_get.js
@@ -20,11 +20,12 @@ window.addEventListener('message', function (e) {
         id_token(e.data.body);
     } else if(e.data.head == "id_token") {
         alert(e.data.body);
+        splatoon_token(e.data.body);
     }
 });
 
 function session_token(session_token_code, session_token_code_verifier) {
-    const form = $('<form style="display: none" action="https://accounts.nintendo.com/connect/1.0.0/api/session_token" method="post" target="session_token_response"></form>');
+    const form = $('<form style="display: none" action="https://accounts.nintendo.com/connect/1.0.0/api/session_token" method="post" target="response"></form>');
     form.append(`<input name="client_id" value="71b963c1b7b6d119"></input>`);
     form.append(`<input name="session_token_code" value="${session_token_code}"></input>`);
     form.append(`<input name="session_token_code_verifier" value="${session_token_code_verifier}"></input>`);
@@ -33,10 +34,22 @@ function session_token(session_token_code, session_token_code_verifier) {
 }
 
 function id_token(session_token) {
-    const form = $('<form style="display: none" action="https://accounts.nintendo.com/connect/1.0.0/api/token" method="post" target="id_token_response"></form>');
+    const form = $('<form style="display: none" action="https://accounts.nintendo.com/connect/1.0.0/api/token" method="post" target="response"></form>');
     form.append(`<input name="client_id" value="71b963c1b7b6d119"></input>`);
     form.append(`<input name="session_token" value="${session_token}"></input>`);
     form.append(`<input name="grant_type" value="urn:ietf:params:oauth:grant-type:jwt-bearer-session-token"></input>`);
+    $('body').append(form);
+    form.submit();
+}
+
+function splatoon_token(id_token) {
+    const form = $('<form style="display: none" action="https://api-lp1.znc.srv.nintendo.net/v3/Account/Login" method="post" target="response"></form>');
+    form.append(`<input name="language" value="ja-JP"></input>`);
+    form.append(`<input name="naBirthday" value="2000-01-01"></input>`);
+    form.append(`<input name="naCountry" value="JP"></input>`);
+    form.append(`<input name="naIdToken" value="${id_token}"></input>`);
+    form.append(`<input name="requestId" value="96a80e43-cc15-4724-9196-31708bc56d1d"></input>`);
+    form.append(`<input name="timestamp" value="${Date.now()}"></input>`);
     $('body').append(form);
     form.submit();
 }
